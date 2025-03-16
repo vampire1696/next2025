5a0ce4b56fa14132a19f7bd3fd6946accb9aef44
vampire1696 <DucHung.Tran@Student.HTW-Berlin.de>
2025-03-17 00:40:46 +0100

    added next auth with google

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { auth, signIn, signOut } from "../../../auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
     <header className="px-5 py-3 bg-white font-sans shadow-md">
-      <nav className="flex justify-between items-center">
+      <nav className="flex justify-between items-center text-black">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
+
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit" className="cursor-pointer">
+                  Signout
+                </button>
+              </form>
+
+              <Link href="">
+                <span>{session?.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google");
+              }}
+            >
+              <button type="submit" className="cursor-pointer">
+                Signin
+              </button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
